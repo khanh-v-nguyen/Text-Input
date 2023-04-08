1fe677db87781b0315fc1eed0b746ce28d2dd83b
Khanh Nguyen <68301461+khanh-v-nguyen@users.noreply.github.com>
2023-04-07 23:49:54 -0700

    Add files via upload

---
+++ files/cmake-build-debug/CMakeFiles/multiText.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/khanh/CLionProjects/multiText copy 5/Application.cpp" "CMakeFiles/multiText.dir/Application.cpp.o" "gcc" "CMakeFiles/multiText.dir/Application.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/ColorChanger.cpp" "CMakeFiles/multiText.dir/ColorChanger.cpp.o" "gcc" "CMakeFiles/multiText.dir/ColorChanger.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/Cursor.cpp" "CMakeFiles/multiText.dir/Cursor.cpp.o" "gcc" "CMakeFiles/multiText.dir/Cursor.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/FileReader.cpp" "CMakeFiles/multiText.dir/FileReader.cpp.o" "gcc" "CMakeFiles/multiText.dir/FileReader.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/GUIComponent.cpp" "CMakeFiles/multiText.dir/GUIComponent.cpp.o" "gcc" "CMakeFiles/multiText.dir/GUIComponent.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/Heap.cpp" "CMakeFiles/multiText.dir/Heap.cpp.o" "gcc" "CMakeFiles/multiText.dir/Heap.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/History.cpp" "CMakeFiles/multiText.dir/History.cpp.o" "gcc" "CMakeFiles/multiText.dir/History.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/KeyShortcuts.cpp" "CMakeFiles/multiText.dir/KeyShortcuts.cpp.o" "gcc" "CMakeFiles/multiText.dir/KeyShortcuts.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/Label.cpp" "CMakeFiles/multiText.dir/Label.cpp.o" "gcc" "CMakeFiles/multiText.dir/Label.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/Letter.cpp" "CMakeFiles/multiText.dir/Letter.cpp.o" "gcc" "CMakeFiles/multiText.dir/Letter.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/MouseEvents.cpp" "CMakeFiles/multiText.dir/MouseEvents.cpp.o" "gcc" "CMakeFiles/multiText.dir/MouseEvents.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/MultiText.cpp" "CMakeFiles/multiText.dir/MultiText.cpp.o" "gcc" "CMakeFiles/multiText.dir/MultiText.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/MultiTextSnapshot.cpp" "CMakeFiles/multiText.dir/MultiTextSnapshot.cpp.o" "gcc" "CMakeFiles/multiText.dir/MultiTextSnapshot.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/NewColorChanger.cpp" "CMakeFiles/multiText.dir/NewColorChanger.cpp.o" "gcc" "CMakeFiles/multiText.dir/NewColorChanger.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/Position.cpp" "CMakeFiles/multiText.dir/Position.cpp.o" "gcc" "CMakeFiles/multiText.dir/Position.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/Snapshot.cpp" "CMakeFiles/multiText.dir/Snapshot.cpp.o" "gcc" "CMakeFiles/multiText.dir/Snapshot.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/SnapshotInterface.cpp" "CMakeFiles/multiText.dir/SnapshotInterface.cpp.o" "gcc" "CMakeFiles/multiText.dir/SnapshotInterface.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/States.cpp" "CMakeFiles/multiText.dir/States.cpp.o" "gcc" "CMakeFiles/multiText.dir/States.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/TextInput.cpp" "CMakeFiles/multiText.dir/TextInput.cpp.o" "gcc" "CMakeFiles/multiText.dir/TextInput.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/TextInputSnapshot.cpp" "CMakeFiles/multiText.dir/TextInputSnapshot.cpp.o" "gcc" "CMakeFiles/multiText.dir/TextInputSnapshot.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/Textbox.cpp" "CMakeFiles/multiText.dir/Textbox.cpp.o" "gcc" "CMakeFiles/multiText.dir/Textbox.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/Typing.cpp" "CMakeFiles/multiText.dir/Typing.cpp.o" "gcc" "CMakeFiles/multiText.dir/Typing.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/Word.cpp" "CMakeFiles/multiText.dir/Word.cpp.o" "gcc" "CMakeFiles/multiText.dir/Word.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/WordSort.cpp" "CMakeFiles/multiText.dir/WordSort.cpp.o" "gcc" "CMakeFiles/multiText.dir/WordSort.cpp.o.d"
+  "/Users/khanh/CLionProjects/multiText copy 5/main.cpp" "CMakeFiles/multiText.dir/main.cpp.o" "gcc" "CMakeFiles/multiText.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ files/cmake-build-debug/CMakeFiles/multiText.dir/MultiTextSnapshot.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/multiText.dir/MultiTextSnapshot.cpp.o: \
+  /Users/khanh/CLionProjects/multiText\ copy\ 5/MultiTextSnapshot.cpp \
+  /Users/khanh/CLionProjects/multiText\ copy\ 5/MultiTextSnapshot.h
